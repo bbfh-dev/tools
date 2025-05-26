33754be1b8292e98e835266c3cf0adcff297afe1
BubbleFish <daforsastudia@gmail.com>
2025-05-26 11:47:14 +0300

    feat: Add mcfunction_build.sh

---
+++ mcfunction_build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+dir=$1
+
+if [[ -f build.sh ]]; then
+    ./build.sh $dir
+    exit 0
+fi
+
+if [[ -f Makefile ]]; then
+    make build
+    exit 0
+fi
+
+echo "build: Skipping..."
