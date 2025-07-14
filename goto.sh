fbaabdbff9356557a0ea19b8c1b42b556646b913
BubbleFish <daforsastudia@gmail.com>
2025-07-14 21:10:45 +0300

    feat: Improve goto.sh

--- goto.sh
+++ goto.sh
@@ -4,40 +4,40 @@
 RECENT=~/.config/shell/recent_project
 
 goto() {
-	# ln -s $1 ~/.local/share/scripts/latest
-	clear
-	rm -rf $RECENT
-	ln -s $1 $RECENT
-	echo "-> $1"
-	cd -P $1
+    # ln -s $1 ~/.local/share/scripts/latest
+    clear
+    rm -rf $RECENT
+    ln -s $1 $RECENT
+    echo "-> $1"
+    cd -P $1
 }
 
 if [[ $1 = "project" ]]; then
-	selected=$({ find $GIT -mindepth 2 -maxdepth 2 -type d,l ; find ~/Documents/Godot -mindepth 1 -maxdepth 1 -type d,l ; } | fzf --scheme=path --algo=v2 --layout=reverse-list --margin 10% --padding 10%)
-	if [[ -z $selected ]]; then
-		return
-	fi
+    selected=$({ find $GIT -mindepth 2 -maxdepth 2 -type d,l ; find ~/Documents/Godot -mindepth 1 -maxdepth 1 -type d,l ; find ~/Public/bookmarks -mindepth 1 -maxdepth 1 -type d,l ; } | fzf --scheme=path --algo=v2 --layout=reverse-list --margin 10% --padding 10%)
+    if [[ -z $selected ]]; then
+        return
+    fi
 
-	if [[ $(basename $selected) == +* || $selected == *.d ]]; then
-		selected=$(find $selected -mindepth 1 -maxdepth 1 -type d,l | fzf --scheme=path --algo=v2 --layout=reverse-list --margin 10% --padding 10%)
-	fi
+    if [[ $(basename $selected) == +* || $selected == *.d ]]; then
+        selected=$(find $selected -mindepth 1 -maxdepth 1 -type d,l | fzf --scheme=path --algo=v2 --layout=reverse-list --margin 10% --padding 10%)
+    fi
 
-	# Check if it's a bare repository
-	if [[ -f $selected/HEAD ]]; then
-		cd -P $selected
-		branch=$(git worktree list | fzf --scheme=path --algo=v2 --layout=reverse-list --margin 10% --padding 10%)
-		read -r first_argument _ <<< "$branch"
-		goto $first_argument
-	else
-		goto $selected
-	fi
+    # Check if it's a bare repository
+    if [[ -f $selected/HEAD ]]; then
+        cd -P $selected
+        branch=$(git worktree list | fzf --scheme=path --algo=v2 --layout=reverse-list --margin 10% --padding 10%)
+        read -r first_argument _ <<< "$branch"
+        goto $first_argument
+    else
+        goto $selected
+    fi
 
-	return
+    return
 fi
 
 if [[ $1 = "recent" ]]; then
-	cd -P $RECENT
-	return
+    cd -P $RECENT
+    return
 fi
 
 echo "Use one of the following commands: project, recent"
